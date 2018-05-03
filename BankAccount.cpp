47e689c1790be49cd0aeceb3c999d89488d04778
Zero0!1 <30416776+MaryZ01@users.noreply.github.com>
2018-05-03 23:39:06 +0300

    Аккаунт з властивостями та конструкторами.
    Добавлено властивості та конструктори.

---
+++ BankAccount.cpp
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Globalization;
+
+namespace Proj1_classes
+{
+    public class Account : IComparable, IFormattable      //public?
+                                                          // ToString(), Tostring(string, IFormatProvider), CompareTo(object)
+    {
+        protected static int count_acc = 0;            //підраховує к-сть об'єктів
+        protected static double commission = 1.75;        //комісія в банку
+
+        protected string ID;
+        protected string Name;
+        protected double Balance;      //{ get; }?
+        protected DateTime CreationDate;
+
+        public Account()
+        {
+            ID = "#";
+            Name = "";
+            Balance = 0;
+            CreationDate = new DateTime();
+            count_acc++;
+        }
+
+        public Account(string id, string name, double _bal, DateTime dt = new DateTime())
+        {
+            ID = id;
+            Name = name;
+            Balance = _bal;
+            CreationDate = dt;
+            count_acc++;
+        }
+    }
+}        
